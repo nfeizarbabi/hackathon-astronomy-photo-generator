fd7f76ab326fb862f895cc77ca9d6e35b57fd8c0
nfeizarbabi <nfeizarbabi@gmail.com>
2024-04-24 10:36:22 -0400

    finalizes styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,5 @@
 const imageContainer = document.querySelector(".main__image");
 
-// async function getRandomPhoto() {
-// 	const randomPhotoUrl =
-// 		"https://api.nasa.gov/planetary/apod?api_key=hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7k&count=1";
-
-// 	try {
-// 		const { data } = await axios.get(randomPhotoUrl);
-// 		const imageUrl = data[0].url;
-// 		// randomPhoto.src = data.url;
-// 		titleElement.textContent = data[0].title;
-// 		dateElement.textContent = data[0].date;
-// 		explanationElement.textContent = data[0].explanation;
-
-// 		// console.log(data);
-// 		imageContainer.src = imageUrl;
-// 	} catch (error) {
-// 		console.error("Error grabbing jpeg:", error);
-// 	}
-// }
 async function getRandomPhoto() {
 	const randomPhotoUrl =
 		"https://api.nasa.gov/planetary/apod?api_key=hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7k&count=1";
