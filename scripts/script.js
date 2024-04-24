8fd1a67452298ea5f3b1561efa37b1daccb0c40d
nfeizarbabi <nfeizarbabi@gmail.com>
2024-04-24 00:35:03 -0400

    updated content functionality, adds styling to rest of the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,27 @@
-console.log("Hello world");
-
-// const apiKey = "hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7k";
-
-// const apiKey = new HackathonSiteApi(
-// 	"api_key=hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7k"
-// );
-
-// async function getRandomPhoto(apiKey) {
-// 	try {
-// 		const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7ks&count=1`;
-
-// 		const response = await axios.get(apiUrl);
-
-// 		const randomPhoto = {
-// 			imageUrl: response.data[0].hdurl, //can also be just .Url <- there are two options for photo url
-// 			title: response.data[0].title,
-// 			date: response.data[0].date,
-// 			explanation: response.data[0].explanation,
-// 		};
-// 		return randomPhoto;
-// 	} catch {
-// 		(error) => {
-// 			console.error("Error getting random photo:", error);
-// 		};
-// 	}
-// }
-
-// button.addEventListener('click', function()) {
-
-// }
-
 const imageContainer = document.querySelector(".main__image");
 
 async function getRandomPhoto() {
-	const randomPhoto =
+	const randomPhotoUrl =
 		"https://api.nasa.gov/planetary/apod?api_key=hobAvyiOoCWbZGTq70P4ZQkEctJmZDXdH2zWNI7k&count=1";
 
-	const { data } = await axios.get(randomPhoto);
-	randomPhoto.src = data.url;
-	console.log(data);
+	try {
+		const { data } = await axios.get(randomPhotoUrl);
+		const imageUrl = data[0].url;
+		// randomPhoto.src = data.url;
+		titleElement.textContent = data[0].title;
+		dateElement.textContent = data[0].date;
+		explanationElement.textContent = data[0].explanation;
+
+		console.log(data);
+		imageContainer.src = imageUrl;
+	} catch (error) {
+		console.error("Error grabbing jpeg:", error);
+	}
 }
 
 getRandomPhoto();
 
 const button = document.querySelector(".main__button");
 const titleElement = document.querySelector(".main__title");
-const dateElement = document.querySelector("main__date");
-const explanationElement = document.querySelector("main__explanation");
+const dateElement = document.querySelector(".main__date");
+const explanationElement = document.querySelector(".main__explanation");
